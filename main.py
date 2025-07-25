a1e3748d6b7c12770a97047971685772ca9b9a41
NU0R <noorosha300@gmail.com>
2025-07-25 20:52:10 +0300

    merge divided

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-first_num=int(input("enter first number"))
-operator=input("enter operator you like (+,-,*,/)")
-second_number=int(input("enter second number"))
-if (operator=="-"):
-    result=first_num - second_number 
-    print("the result is", result)
-elif(operator=="/"):
-    result=first_num / second_number
-    print("the result is", result)
-=======
 first_number=float(input("enter the first number"))
 operator=input("enter the operator you want (+,-,*,/)")
 second_number=float(input("enter the second number"))
@@ -18,4 +7,9 @@ if (operator== "+"):
 elif (operator== "*"):
     result=first_number * second_number
     print("the result is", result)    
->>>>>>> fc94e34ccc257e2cca5798327ab5097eed67bb22
+elif (operator=="-"):
+    result=first_num - second_number 
+    print("the result is", result)
+elif(operator=="/"):
+    result=first_num / second_number
+    print("the result is", result)
