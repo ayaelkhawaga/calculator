fc94e34ccc257e2cca5798327ab5097eed67bb22
ayaelkhawaga <ayaelkhawaga606@gmail.com>
2025-07-25 20:39:21 +0300

    added plus and times

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 first_number=float(input("enter the first number"))
+operator=input("enter the operator you want (+,-,*,/)")
 second_number=float(input("enter the second number"))
-result=first_number + second_number
-print("the result is", result) 
+if (operator== "+"):
+    result=first_number + second_number
+    print("the result is", result) 
+elif (operator== "*"):
+    result=first_number * second_number
+    print("the result is", result)    
